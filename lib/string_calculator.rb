502dda6451a9924ca601f261ef08eab14da4b603
prince agrahari <agrahariprin7991@gmail.com>
2024-12-22 21:38:13 +0530

    Add the logics for writing the test cases

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,20 @@
 class StringCalculator
+  def self.add(numbers)
+    return 0 if numbers.empty?
+
+    delimiter = /,|\n/
+
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2..])
+      numbers = parts[1]
+    end
+
+    numbers = numbers.split(/#{delimiter}/).map(&:to_i)
+
+    negatives = numbers.select { |n| n < 0 }
+    raise "negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
+    numbers.sum
+  end
 end
